1f9474a18b41ad856920aa161fe7cbffc3cb32f3
Sheetal Taank <sheetaltaank74@email.com>
2025-07-25 12:31:35 +0530

    update files

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
-let todo =[];
-let req = prompt("please enter your request");
+let inp=document.querySelector("input");
+let btn=document.querySelector("button");
 
-while(true){
-    if(req=="quit"){
-        console.log("quitting app");
-        break;
-    }
-    if(req=="list"){
-        console.log("----------");
-        for(let i=0;i<todo.length;i++){
-            console.log(i,todo[i]);
-        }
-        console.log("----------");
+let ul=document.querySelector("ul");
 
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
 
-    }
-    else if(req=="add"){
-        let task=prompt("please enter task you want to add");
-        todo.push(task);
-        console.log("task added");
+    let dltbtn=document.createElement("button");
+    dltbtn.innerText="Delete";
+    dltbtn.classList.add("delete");
+    item.appendChild(dltbtn);
+
+    ul.appendChild(item);
+console.log(inp.value);
+inp.value="";
+});
 
 
-    }
-    
-    else if(req=="delete"){
-        let ind=prompt("please enter index of task you want to remove");
-        todo.splice(ind,1);
-    }
-    else {
-        console.log("wrong request !");
-    }
-    req=prompt("please enter your request");
 
-}
+// let dltbtns = document.querySelectorAll(".delete");
+// for( btn of dltbtns){
+//     btn.addEventListener("click",function(){
+//         let parent = this.parentElement;
+//         parent.remove();
+
+//      // these event listeners work only for the existing list items
+//      // and not for the newly added ones
+     
+//     });
+// }
+
+
+
+
+//for solving this prblm , we use event delegation 
+// event delegation is performed by using the phenomenon of bubbling
+// isme hum jis child elemnt par event apply krna chahte hain , uske parent element ke upar vo event lga 
+// dete hain , so that child elemnt par bhi vo event apne app trigger ho jayee
+
+// it means hum buttons k upar nhi blki inke parents - list items ya ul k upar event listener add krenge
+
+
+
+ul.addEventListener("click",function(event){
+   if(event.target.nodeName == "BUTTON"){
+    let listItem = event.target.parentElement;
+    listItem.remove();
+    console.log("deleted");
+   }
+});
+
